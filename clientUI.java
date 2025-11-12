483483bb06e04fe5cdcadfc8d3a16a08a2d5fc4c
Herodes <a106927@alunos.uminho.pt>
2025-11-12 18:34:52 +0000

    cenas básicas do clientUI

--- clientUI.java
+++ clientUI.java
@@ -1,5 +1,16 @@
 //nome auto explicativo
+import java.io.IOException;
+import java.util.*;
+
 
 public class clientUI {
-    
+    private boolean authenticated = false;
+    //private TimeSeriesClient client;
+    private Scanner scanner;
+
+    public clientUI(String host, int port) throws IOException {
+        //this.client = new TimeSeriesClient(host, port);
+        this.scanner = new Scanner(System.in);
+        //client.connect();
+    }
 }
